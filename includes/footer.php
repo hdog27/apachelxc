b14c1b3e13c14b4f15be78b0ae6e09211e769863
Harrison <hmax2748@gmail.com>
2026-09-07 19:05:30 -0400

    Cyber Lab: preload HQ globe texture hook before route animation

--- includes/footer.php
+++ includes/footer.php
@@ -11,6 +11,9 @@
 <p class="site-credit">Built by <strong>Harrison</strong>, <a href="https://github.com/1Fragz1" target="_blank" rel="noopener">Zachary Lavornia</a> and <a href="https://www.linkedin.com/in/morgan-stone-90b514419/" target="_blank" rel="noopener">Morgan Stone</a></p>
 <script src="/js/kitty.js?v=<?= @filemtime(__DIR__ . "/../js/kitty.js") ?>"></script>
 <script src="/js/main.js?v=<?= @filemtime(__DIR__ . "/../js/main.js") ?>"></script>
+<?php if (($body_class ?? '') === 'cyberlab'): ?>
+<script src="/js/globe-hq-pre.js?v=<?= @filemtime(__DIR__ . "/../js/globe-hq-pre.js") ?>"></script>
+<?php endif; ?>
 <?php if (!empty($page_js)) foreach ($page_js as $js): ?>
 <script src="/js/<?= htmlspecialchars($js) ?>?v=<?= @filemtime(__DIR__ . "/../js/" . $js) ?>"></script>
 <?php endforeach; ?>
