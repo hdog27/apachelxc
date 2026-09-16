16aa6df7f0b662e35be5a256f6d844a6e6ea96de
Harrison <hmax2748@gmail.com>
2026-09-16 18:46:07 -0400

    Load desktop nav liquid glass renderer

--- includes/footer.php
+++ includes/footer.php
@@ -28,6 +28,7 @@
 <script src="/js/cyberlab-layout.js?v=<?= @filemtime(__DIR__ . "/../js/cyberlab-layout.js") ?>"></script>
 <?php endif; ?>
 <script src="/js/liquid-glass-webgl.js?v=<?= @filemtime(__DIR__ . "/../js/liquid-glass-webgl.js") ?>"></script>
+<script src="/js/liquid-glass-nav-webgl.js?v=<?= @filemtime(__DIR__ . "/../js/liquid-glass-nav-webgl.js") ?>"></script>
 <script src="/js/liquid-glass-debug.js?v=<?= @filemtime(__DIR__ . "/../js/liquid-glass-debug.js") ?>"></script>
 </body>
 </html>
